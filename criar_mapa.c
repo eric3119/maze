484eccd66ba26e9aad0058bb0cab6a360f9fc66b
ericCoelho <eric.s.coelho@gmail.com>
2017-10-08 20:18:44 -0300

    Update criar_mapa.c

--- criar_mapa.c
+++ criar_mapa.c
@@ -3,34 +3,34 @@
 #define ESQ 2
 #define DIR 3
 
-void verif_mapa(char mapa[TAM][TAM], int *i, int *j, int *disp){
+void verif_mapa(struct var *variaveis, int *disp){
 	
 	int k;
 	
 	for (k=0; k<4; k++) disp[k]=0;
 
-	if ((*i-2>0)&&(mapa[*i-2][*j]=='0'))
+	if (((variaveis->i)-2>0)&&(variaveis->mapa[(variaveis->i)-2][variaveis->j]=='0'))
 		disp[ESQ]=1;	
 
-	if((*i+2<TAM)&&(mapa[*i+2][*j]=='0'))
+	if(((variaveis->i)+2<TAM)&&(variaveis->mapa[(variaveis->i)+2][variaveis->j]=='0'))
 		disp[DIR]=1;
 
-	if((*j-2>0)&&(mapa[*i][*j-2]=='0'))
+	if((variaveis->j-2>0)&&(variaveis->mapa[(variaveis->i)][variaveis->j-2]=='0'))
 		disp[CIMA]=1;
 
-	if((*j+2<TAM)&&(mapa[*i][*j+2]=='0'))
+	if((variaveis->j+2<TAM)&&(variaveis->mapa[(variaveis->i)][variaveis->j+2]=='0'))
 		disp[BAIXO]=1;
 }
 
-void criar_mapa(char mapa[TAM][TAM], int *i, int *j, int aux){
+void criar_mapa(struct var *variaveis){
 
 	int escolha, disp[4], k, prog, possDisp;
 
-	mapa[*i][*j]=' ';
+	variaveis->mapa[(variaveis->i)][variaveis->j]=' ';
 
-	verif_mapa(mapa, i, j, disp);
-	desenhar_mapa(mapa);
-	progresso(mapa);		
+	verif_mapa(variaveis, disp);
+	desenhar_mapa(variaveis->mapa);
+	progresso(variaveis->mapa);		
 
 	do{
 		possDisp=0;
@@ -41,50 +41,50 @@ void criar_mapa(char mapa[TAM][TAM], int *i, int *j, int aux){
 		if(possDisp==0)
 			break;
 		else if(possDisp<=2)
-			aux=4;
+			variaveis->aux=4;
 
 		for(k=0; k<4000000; k++);
 
-		do{	
+		do{	// escolha do numero aleatorio
 			escolha = rand()%4;
-		}while(escolha == aux);
+		}while(escolha == variaveis->aux);
 
 		if((escolha==CIMA)&&disp[CIMA]){
 
 			disp[CIMA]=0;
-			mapa[*i][*j-1]=' ';
-				aux = escolha;
-				(*j)-=2;
-				criar_mapa(mapa, i, j, aux);
-				(*j)+=2;
+			variaveis->mapa[(variaveis->i)][variaveis->j-1]=' ';
+				variaveis->aux = escolha;
+				(variaveis->j)-=2;
+				criar_mapa(variaveis);
+				(variaveis->j)+=2;
 		}
 		if((escolha==BAIXO)&&disp[BAIXO]){
 
 			disp[BAIXO]=0;
-			mapa[*i][*j+1]=' ';
-				aux = escolha;
-				(*j)+=2;
-				criar_mapa(mapa, i, j, aux);
-				(*j)-=2;
+			variaveis->mapa[(variaveis->i)][variaveis->j+1]=' ';
+				variaveis->aux = escolha;
+				(variaveis->j)+=2;
+				criar_mapa(variaveis);
+				(variaveis->j)-=2;
 		}
 		if((escolha==ESQ)&&disp[ESQ]){
 
 			disp[ESQ]=0;
-			mapa[*i-1][*j]=' ';
-				aux = escolha;
-				(*i)-=2;
-				criar_mapa(mapa, i, j, aux);
-				(*i)+=2;
+			variaveis->mapa[((variaveis->i))-1][variaveis->j]=' ';
+				variaveis->aux = escolha;
+				((variaveis->i))-=2;
+				criar_mapa(variaveis);
+				((variaveis->i))+=2;
 		}
 		if((escolha==DIR)&&disp[DIR]){
 
 			disp[DIR]=0;
-			mapa[*i+1][*j]=' ';
-				aux = escolha;
-				(*i)+=2;
-				criar_mapa(mapa, i, j, aux);
-				(*i)-=2;
+			variaveis->mapa[(variaveis->i)+1][variaveis->j]=' ';
+				variaveis->aux = escolha;
+				(variaveis->i)+=2;
+				criar_mapa(variaveis);
+				(variaveis->i)-=2;
 		}
-		verif_mapa(mapa, i, j, disp);		
+		verif_mapa(variaveis, disp);		
 	}while(disp[escolha]== 0);
 }
